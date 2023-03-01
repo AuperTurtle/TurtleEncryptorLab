587bdc6ede1ae176f1f0ad14f566d4e896d93e62
1E10 Laptop <sample@bths.edu>
2023-03-01 10:53:40 -0500

    .

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -1,14 +1,22 @@
 public class Encryptor {
-    /** A two-dimensional array of single-character strings, instantiated in the constructor */
+    /**
+     * A two-dimensional array of single-character strings, instantiated in the constructor
+     */
     private String[][] letterBlock;
 
-    /** The number of rows of letterBlock, set by the constructor */
+    /**
+     * The number of rows of letterBlock, set by the constructor
+     */
     private int numRows;
 
-    /** The number of columns of letterBlock, set by the constructor */
+    /**
+     * The number of columns of letterBlock, set by the constructor
+     */
     private int numCols;
 
-    /** Constructor*/
+    /**
+     * Constructor
+     */
     public Encryptor(int r, int c) {
         letterBlock = new String[r][c];
         numRows = r;
@@ -19,13 +27,14 @@ public class Encryptor {
         return letterBlock;
     }
 
-    /** Places a string into letterBlock in row-major order.
+    /**
+     * Places a string into letterBlock in row-major order.
      *
-     *   @param str  the string to be processed
-     *
-     *   Postcondition:
-     *     if str.length() < numRows * numCols, "A" in each unfilled cell
-     *     if str.length() > numRows * numCols, trailing characters are ignored
+     * @param str the string to be processed
+     *            <p>
+     *            Postcondition:
+     *            if str.length() < numRows * numCols, "A" in each unfilled cell
+     *            if str.length() > numRows * numCols, trailing characters are ignored
      */
     public void fillBlock(String str) {
         int i = 0;
@@ -39,7 +48,7 @@ public class Encryptor {
         }
         for (int a = 0; a < numRows; a++) {
             for (int b = 0; b < numCols; b++) {
-                if (letterBlock[a][b] == null) {
+                if (letterBlock[a][b] == null || letterBlock[a][b].equals("*")) {
                     letterBlock[a][b] = "A";
                 }
             }
@@ -47,11 +56,12 @@ public class Encryptor {
     }
 
 
-    /** Extracts encrypted string from letterBlock in column-major order.
+    /**
+     * Extracts encrypted string from letterBlock in column-major order.
+     * <p>
+     * Precondition: letterBlock has been filled
      *
-     *   Precondition: letterBlock has been filled
-     *
-     *   @return the encrypted string from letterBlock
+     * @return the encrypted string from letterBlock
      */
     public String encryptBlock() {
         String tempString = "";
@@ -64,49 +74,76 @@ public class Encryptor {
     }
 
 
-    /** Encrypts a message.
+    /**
+     * Encrypts a message.
      *
-     *  @param message the string to be encrypted
-     *
-     *  @return the encrypted message; if message is the empty string, returns the empty string
+     * @param message the string to be encrypted
+     * @return the encrypted message; if message is the empty string, returns the empty string
      */
     public String encryptMessage(String message) {
         String tempString = "";
+        String tempString2 = "";
         int arrSize = numRows * numCols;
         int tempNum = 0;
-        numRows = 2;
-        numCols = 3;
-        while (tempNum < message.length()) {
-            fillBlock(message.substring(tempNum));
+        while (tempNum < message.length() - arrSize) {
+            fillBlock(message.substring(tempNum, tempNum + arrSize));
             tempString += encryptBlock();
             tempNum += arrSize;
         }
+        for (int i = 0; i < arrSize; i++) {
+            tempString2 += "*";
+        }
+        fillBlock(tempString2);
+        fillBlock(message.substring(tempNum));
+        tempString += encryptBlock();
         return tempString;
     }
 
-    /**  Decrypts an encrypted message. All filler 'A's that may have been
-     *   added during encryption will be removed, so this assumes that the
-     *   original message (BEFORE it was encrypted) did NOT end in a capital A!
+    /**
+     * Decrypts an encrypted message. All filler 'A's that may have been
+     * added during encryption will be removed, so this assumes that the
+     * original message (BEFORE it was encrypted) did NOT end in a capital A!
+     * <p>
+     * NOTE! When you are decrypting an encrypted message,
+     * be sure that you have initialized your Encryptor object
+     * with the same row/column used to encrypted the message! (i.e.
+     * the “encryption key” that is necessary for successful decryption)
+     * This is outlined in the precondition below.
+     * <p>
+     * Precondition: the Encryptor object being used for decryption has been
+     * initialized with the same number of rows and columns
+     * as was used for the Encryptor object used for encryption.
      *
-     *   NOTE! When you are decrypting an encrypted message,
-     *         be sure that you have initialized your Encryptor object
-     *         with the same row/column used to encrypted the message! (i.e.
-     *         the “encryption key” that is necessary for successful decryption)
-     *         This is outlined in the precondition below.
-     *
-     *   Precondition: the Encryptor object being used for decryption has been
-     *                 initialized with the same number of rows and columns
-     *                 as was used for the Encryptor object used for encryption.
-     *
-     *   @param encryptedMessage  the encrypted message to decrypt
-     *
-     *   @return  the decrypted, original message (which had been encrypted)
-     *
-     *   TIP: You are encouraged to create other helper methods as you see fit
-     *        (e.g. a method to decrypt each section of the decrypted message,
-     *         similar to how encryptBlock was used)
+     * @param encryptedMessage the encrypted message to decrypt
+     * @return the decrypted, original message (which had been encrypted)
+     * <p>
+     * TIP: You are encouraged to create other helper methods as you see fit
+     * (e.g. a method to decrypt each section of the decrypted message,
+     * similar to how encryptBlock was used)
      */
     public String decryptMessage(String encryptedMessage) {
-        return "";
+        String tempString = "";
+        int arrSize = numRows * numCols;
+        int tempNum = 0;
+        while (tempNum < encryptedMessage.length()) {
+        for (int a = 0; a < numCols; a++) {
+            for (int b = 0; b < numRows; b++) {
+                letterBlock[b][a] = String.valueOf(encryptedMessage.charAt(tempNum));
+                tempNum++;
+            }
+        }
+        for (int a = 0; a < numRows; a++) {
+            for (int b = 0; b < numCols; b++) {
+                tempString += letterBlock[a][b];
+            }
+        }
+        }
+        int tempNum2 = tempString.length() - 1;
+        while (String.valueOf(tempString.charAt(tempNum2)).equals("A")) {
+            tempNum2--;
+        }
+        tempString = tempString.substring(0, tempNum2 + 1);
+        return tempString;
     }
+
 }
